508f77b1324f3f3eb9212a9ff49a2c9305f0070f
Root(김명석) <mskim@gabia.com>
2021-06-18 12:49:36 +0900

    modify: maven project -> gradle project

--- src/main/java/com/gabia/discovery/DiscoveryApplication.java
+++ src/main/java/com/gabia/discovery/DiscoveryApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServiceApplication {
+@SpringBootApplication
+public class DiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceApplication.class, args);
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
 
 }
